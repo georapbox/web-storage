a63a4efe9e0cd27ab1beab41a1d661cddc6bb271
George Raptis <georapbox@gmail.com>
2020-04-08 17:44:46 +0300

    Update linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,11 +14,27 @@ module.exports = {
     'eslint:recommended'
   ],
   rules: {
-    'indent': ['warn', 2, {
-      'SwitchCase': 1
-    }],
-    'eqeqeq': ['error', 'always', {
-      'null': 'ignore'
-    }]
+    // Possible Errors
+    'no-console': 'off',
+    'no-empty': ['error', { 'allowEmptyCatch': true }],
+
+    // Stylistic Issues
+    'indent': ['warn', 2, { 'SwitchCase': 1, 'ignoredNodes': ['TemplateLiteral'] }],
+    'quotes': ['warn', 'single', { 'allowTemplateLiterals': true }],
+    'brace-style': ['warn'],
+    'no-multiple-empty-lines': ['warn', { 'max': 1 }],
+    'no-trailing-spaces': ['warn'],
+    'no-unneeded-ternary': ['warn'],
+    'comma-spacing': ['warn'],
+    'comma-style': ['warn'],
+    'comma-dangle': ['warn'],
+    'eol-last': ['warn'],
+    'jsx-quotes': ['warn', 'prefer-double'],
+    'semi': ['warn', 'always'],
+
+    // Best Practices
+    'eqeqeq': ['error', 'always', { 'null': 'ignore' }],
+    'no-multi-spaces': ['warn', { 'ignoreEOLComments': true }],
+    'curly': ['warn']
   }
 };
